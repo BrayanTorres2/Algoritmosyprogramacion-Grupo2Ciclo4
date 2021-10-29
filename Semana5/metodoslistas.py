b31a9da291b5056f0f7899041a58da13153ce351
Brayan Torres <47356178+BrayanTorres2@users.noreply.github.com>
2021-10-29 12:29:21 -0500

    Create metodoslistas.py

---
+++ Semana5/metodoslistas.py
@@ -0,0 +1,19 @@
+#Métodos de las listas
+#append()
+#clear()
+#extend() une una lista con otra
+#count()
+#index()
+#insert(posicion,elemento)
+#pop()
+#remove()
+#reverse()
+#sort()
+#.sort(reverse=True)
+#lista
+lista=[10,20,30,2,4,5,-102]
+lista_dos=["a","b","c","a","a","z","w","f"]
+#print(len(lista))
+lista_dos.sort()
+#lista.sort(reverse=True)
+print(lista_dos)
